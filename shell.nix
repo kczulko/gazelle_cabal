5bb6e935a00106229a23fe1529a6a3e4771a9a0d
kczulko <karol.czulkowski@protonmail.com>
2021-10-28 16:50:13 +0200

    Example + problem desc

--- shell.nix
+++ shell.nix
@@ -9,6 +9,11 @@ mkShell {
   # See: https://github.com/bazelbuild/bazel/issues/4231
   BAZEL_USE_CPP_ONLY_TOOLCHAIN=1;
   buildInputs = [
+    zlib
+    zlib.dev
+    ghc
+    cabal-install
+    haskell-language-server
     bazel_4
     binutils
     cacert
